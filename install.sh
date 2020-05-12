acaf0fcc35af5735cdb13b2eeb84407f47376a7e
Alvaro Mouriño <alvaro@mourino.net>
2020-05-12 15:24:13 +0200

    Add wget

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ case `uname` in
       /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
     fi
 
-    PACKAGES="awscli git goenv htop ipython jq k9s n tfenv tgenv tmux zsh speedtest-cli"
+    PACKAGES="awscli git goenv htop ipython jq k9s n tfenv tgenv tmux zsh speedtest-cli wget"
     for PKG in $PACKAGES; do
       if ! brew list $PKG > /dev/null; then
         if [ "$PKG" = "tgenv" ]; then
